087a08ffe47bc0c7e4f34033c71f014925a46d5a
David <david.cnvs@gmail.com>
2022-07-11 01:24:50 -0400

    wip login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,24 +1,49 @@
 import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from "react-router-dom"
+import { useForm } from "../../hooks"
 import { AuthLayout } from "../layout/AuthLayout"
 
 export const LoginPage = () => {
+
+  const {email, password, onInputChange} = useForm({
+    email: 'david.cnvs@gmail.com',
+    password: '123456',
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({email, password});
+  }
+
   return (
-    
     <AuthLayout title='Login'>
-        <form>
+        <form onSubmit={onSubmit}>
           <Grid container>
             <Grid item xs={12} sx={{mt:2}}>
-              <TextField label='email' type='email' placeholder='email@gmail.com' fullWidth />
+              <TextField 
+              label='email' 
+              type='email' 
+              placeholder='email@gmail.com' 
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange} />
             </Grid>
             <Grid item xs={12} sx={{mt:2}}>
-              <TextField label='password' type='password' placeholder='*******' fullWidth />
+              <TextField 
+              label='password' 
+              type='password' 
+              placeholder='*******' 
+              fullWidth 
+              name="password"
+              value={password}
+              onChange={onInputChange} />
             </Grid>
 
             <Grid container spacing={2} sx={{mb:2, mt:1}}>
               <Grid item xs={12} sm={6}>
-                <Button variant="contained" fullWidth>
+                <Button type="submit" variant="contained" fullWidth>
                   <Typography>Login</Typography>
                 </Button>
               </Grid>
