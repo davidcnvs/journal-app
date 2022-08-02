1f2b46b615fbfa244008bb0cbc6cc5f071d98d39
David Canovas <david.canovas@genesys.cl>
2022-08-02 12:51:18 -0400

    add loader status cheking

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,24 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Routes, Route, Link } from "react-router-dom";
 import { AuthRouter } from "../auth/routes/AuthRouter";
+import { FirebaseApp, FirebaseAuth } from '../firebase/config';
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+
+  const {status} = useSelector(state => state.auth);
+
+  useEffect( () => {
+    onAuthStateChanged(FirebaseAuth);
+  }, [])
+    
+  if(status === 'checking') {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
         {/* Login y Registro */}
